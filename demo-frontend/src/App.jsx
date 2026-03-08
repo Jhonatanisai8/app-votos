9f5f83c02833a9367ea244cac1ce679dabdbffe9
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-03-08 00:16:46 -0500

    initialize React application with basic routing for login and signup.

--- demo-frontend/src/App.jsx
+++ demo-frontend/src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Header from "./pages/header/Header";
+import Login from "./pages/auth/login/Login";
+import Signup from "./pages/auth/signup/Signup";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <Header />
+      <Routes>
+        <Route path="/registro" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
