5f95f720dcc3709524cc87a99cb26596634a16c2
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-03-08 00:30:34 -0500

    Initialize React frontend project with core dependencies and a basic header component.

--- demo-frontend/src/pages/header/Header.jsx
+++ demo-frontend/src/pages/header/Header.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
-
+import React from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              App Votos
+            </Typography>
+            <Button component={Link} to="/login" color="inherit">
+              Login
+            </Button>
+            <Button component={Link} to="/registro" color="inherit">
+              Registro
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+};
 
-export default Header
+export default Header;
