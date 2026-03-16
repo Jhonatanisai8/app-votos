be376a4f821e845a6ff58f8b1e431c529bdc7430
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-03-16 09:40:58 -0500

    feat: Implementar la entidad `Usuario` y `UsuarioServiceImpl` para admitir detalles de usuario y autenticación JWT.

--- demo-backend/src/main/java/com/isai/demobackend/entities/Usuario.java
+++ demo-backend/src/main/java/com/isai/demobackend/entities/Usuario.java
@@ -7,10 +7,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
-public class Usuario {
+public class Usuario implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +26,34 @@ public class Usuario {
   private String nombres;
   private String apellidos;
   private UsuarioRol rol;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority(rol.name()));
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
--- demo-backend/src/main/java/com/isai/demobackend/services/jwt/UsuarioServiceImpl.java
+++ demo-backend/src/main/java/com/isai/demobackend/services/jwt/UsuarioServiceImpl.java
@@ -1,14 +1,26 @@
 package com.isai.demobackend.services.jwt;
 
+import com.isai.demobackend.repositories.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UsuarioServiceImpl implements UsuarioService {
+
+  private final UsuarioRepository usuarioRepository;
+
   @Override
   public UserDetailsService userDetailsService() {
-    return null;
+    return new UserDetailsService() {
+      @Override
+      public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return usuarioRepository.findFirstByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado "));
+      }
+    };
   }
 }
