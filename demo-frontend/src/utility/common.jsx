d8cb18c501fd71e65c0a017e402f85203ee4721d
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-03-16 16:44:30 -0500

    Implementar la configuración de seguridad basada en JWT en el backend y agregar utilidades de gestión de tokens al frontend.

--- demo-frontend/src/utility/common.jsx
+++ demo-frontend/src/utility/common.jsx
@@ -1,5 +1,14 @@
 import Cookies from "js-cookie";
 const TOKEN = "token";
+
 export const guardarToken = (token) => {
   Cookies.set(TOKEN, token, { expires: 7 });
+};
+
+export const obtenerToken = () => {
+  return Cookies.get(TOKEN) || null;
+};
+
+export const eliminarToken = () => {
+  Cookies.remove(TOKEN);
 };
